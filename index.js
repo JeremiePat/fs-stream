825b54f178cd2671a9d047a577086f27eaecad93
Jeremie Patonnier <jpatonnier@clever-age.com>
2016-01-03 23:32:46 +0100

    Basic module set up.

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Modules
+// ----------------------------------------------------------------------------
+var fs      = require('fs');
+var through = require('through2');
+var gs      = require('glob-stream');
+
+// API
+// ----------------------------------------------------------------------------
+function streamer(globPatern, options) {
+  return gs.create(globPatern, options)
+    .pipe(through.obj(function (chunk, enc, cb) {
+      fs.stat(chunk.path, function (err, stats) {
+        if (err) { cb(err); return; }
+
+        cb(null, {
+          cwd   : chunk.cwd,
+          path  : chunk.path,
+          stats : stats
+        });
+      });
+    }));
+}
+
+module.exports = streamer;
---
+++ readme.md
@@ -0,0 +1,12 @@
+fs-stream
+===============================================================================
+
+A wrapper and a set of utilities around [glob-stream](https://www.npmjs.com/package/glob-stream) to handle simple files manipulation through streams.
+
+API
+-------------------------------------------------------------------------------
+
+### _module_(globPattern, [options])
+
+Return a stream of file and dir. For a full documentation of `options` and glob pattern, go read the [glob-stream documentation](https://github.com/gulpjs/glob-stream).
+
